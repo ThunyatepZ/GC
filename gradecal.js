8ead9e60d423cb0e0265680e09218d6994b16ea5
ThunyatepZ <taptitan4@gmail.com>
2022-08-01 23:53:26 +0700

    Sum

---
+++ gradecal.js
@@ -0,0 +1,16 @@
+var IP = 50;
+if (IP >= 90) {
+    console.log("A");
+}
+else if (IP >= 80) {
+    console.log("B");
+}
+else if (IP >= 70) {
+    console.log("C");
+}
+else if (IP >= 60) {
+    console.log("D");
+}
+else {
+    console.log("E");
+}
